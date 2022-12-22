f218c36631911db921c5a878dccc6a099bf14682
Sagar <Sagararyal1122@gmail.com>
2022-12-22 17:44:55 +0545

    Implement map

--- lib/map.dart
+++ lib/map.dart
@@ -17,21 +17,36 @@ class _BrozoMapState extends State<BrozoMap> {
 
   static const CameraPosition _kLake = CameraPosition(
       // bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(28.209620, 83.985523),
       // tilt: 59.440717697143555,
       zoom: 14);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
+
+  List<Marker> _marker = [];
+  List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(28.209620, 83.985523),
+        infoWindow: InfoWindow(title: "My position"))
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-  
-        // compassEnabled: true,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
         initialCameraPosition: _kLake,
+        markers: Set<Marker>.of(_marker),
         mapType: MapType.normal,
+        myLocationButtonEnabled: true,
       ),
     );
   }
